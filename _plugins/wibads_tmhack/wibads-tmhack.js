7d2667fa502da9e01e68469ce5fd74a2cbe44519
bogdan-nazar <me@bogdan-nazar.ru>
2014-07-01 17:15:08 +0400

    1. New background block added. 2. Wibads hack updated to 1.0.4b2

--- _plugins/wibads_tmhack/wibads-tmhack.js
+++ _plugins/wibads_tmhack/wibads-tmhack.js
@@ -1,36 +1,14 @@
 /*
  * Плагин WibAds Timeout Hack
- * Версия: 1.0.2 (27.08.2013 12:43 +0400)
+ * Версия: 1.0.4 build 2 (01.07.2014 16:23 +0400)
  * Developer: Bogdan Nazar <nazar.bogdan@gmail.com>
  * Copyright (c) 2005-2013 Metro
  *
  * Требования:  Metro Core Thirdparty
  */
-
- // Unregistering listener
-(function(){
-	try {
-		var l = wib_gallery_paging_slideshow_event.callbacks.length;
-		if (l) {
-			for (var p in wib_gallery_paging_slideshow_event.callbacks) {
-				if (wib_gallery_paging_slideshow_event.callbacks[p] == wib_ads.reload) {
-					wib_gallery_paging_slideshow_event.callbacks.splice(p, 1);
-					break;
-				}
-			}
-		}
-	} catch (e) {
-		console.log("Can't unregister callback wib_ads.reload: " + e.message + " [" + e.name + "/" + e.type + "]");
-	}
-})();
-
-function WibAds() {
-	var _lastReloaded = {
-		adcomp18: {
-			tm: 0,
-			ival: 30000
-		}
-	};
+function WibAdsHack() {
+	var _reloadTm = 30000;
+	var _lastReloaded = {};
 	var reloadable_ads;
 	var gallery_overlay_ad;
 	var gallery_overlay_ad_html =
@@ -45,7 +23,7 @@ function WibAds() {
 	"</div>";
 
 	var append_fif = function(ad) {
-	var iframe = document.createElement("iframe");
+		var iframe = document.createElement("iframe");
 		iframe.style.margin = "0px";
 		iframe.style.padding = "0px";
 		iframe.style.borderWidth = "0px";
@@ -62,11 +40,16 @@ function WibAds() {
 		ad_expander.append(iframe);
 	}
 
-
 	// API
 	// Constructor function
 	this.init = function() {
 		reloadable_ads = new Array();
+		if (typeof wib_ads_thirdparty != "undefined") {
+			for (c in wib_ads_thirdparty.ads) {
+				if (!wib_ads_thirdparty.ads.hasOwnProperty(c)) continue;
+				reloadable_ads.push(wib_ads_thirdparty.ads[c]);
+			}
+		}
 	}
 
 	// Reload a specific or all reloadable ads
@@ -84,46 +67,50 @@ function WibAds() {
 			append_fif(gallery_overlay_ad);
 		} else {
 			for (var i = 0; i < reloadable_ads.length; i++) {
-				if (reloadable_ads[i].pos == "adcomp-18") {
+				if (!reloadable_ads.hasOwnProperty(i)) continue;
+				if ((typeof reloadable_ads[i].tm_hack == "boolean") && reloadable_ads[i].tm_hack) {
 					var geoSPT = false;
-					if (typeof thirdparty_core != "undefined") {
-						try {
-							var g = thirdparty_core.pluginGet("iptogeo");
-							var geo = g.getData(false);
-							if ((typeof geo == "object") && (geo) && (typeof geo.region != "undefined")) {
-								if ((geo.region == "Санкт-Петербург") || (geo.region == "Ленинградская область")) geoSPT = true;
-							}
-						} catch(e){}
+					if ((typeof reloadable_ads[i].geo_spt == "boolean") && reloadable_ads[i].geo_spt) {
+						if (typeof thirdparty_core != "undefined") {
+							try {
+								var g = thirdparty_core.pluginGet("iptogeo");
+								var geo = g.getData(false);
+								if ((typeof geo == "object") && (geo) && (typeof geo.region != "undefined")) {
+									if ((geo.region == "Санкт-Петербург") || (geo.region == "Ленинградская область")) geoSPT = true;
+								}
+							} catch(e){}
+						}
 					}
-					if (geoSPT) {
-						append_fif(reloadable_ads[i]);
-					} else {
+					if (geoSPT) append_fif(reloadable_ads[i]);
+					else {
 						try {
 							var t = new Date().getTime();
-							if ((t - _lastReloaded.adcomp18.tm) > _lastReloaded.adcomp18.ival) {
-								_lastReloaded.adcomp18.tm = t;
+							if (typeof _lastReloaded[reloadable_ads[i].pos] != "object") {
+								_lastReloaded[reloadable_ads[i].pos] = {tm: 0, ival: ((typeof reloadable_ads[i].ival == "number") ? reloadable_ads[i].ival : _reloadTm)};
+							}
+							if ((t - _lastReloaded[reloadable_ads[i].pos].tm) > _lastReloaded[reloadable_ads[i].pos].ival) {
+								_lastReloaded[reloadable_ads[i].pos].tm = t;
 								append_fif(reloadable_ads[i]);
 							} else return;
 						} catch(e) {
 							append_fif(reloadable_ads[i]);
-							console.log("Can't check wib_ads timeout for adcomp-18: " + e.message + " [" + e.name + "/" + e.type + "]");
+							console.log("Can't check wib_ads timeout for " + reloadable_ads[i].pos + ": [" + e.name + "/" + e.message + "]");
 						}
 					}
 				} else append_fif(reloadable_ads[i]);
 			}
 		}
 	}
-
 	// Initiate the reloadable ads
 	this.set_reloadable_ad = function(provider, pos, src, width, height) {
-		var reloadable_ad = {
+		if ((typeof provider == "object") && provider) reloadable_ads.push(provider);
+		else reloadable_ads.push({
 			"provider": provider,
 			"pos": pos,
 			"src": src,
 			"width": width,
 			"height": height
-		};
-		reloadable_ads.push(reloadable_ad);
+		});
 	}
 	// Changin reload timeout
 	this.set_reload_timeout = function(adname, timeout) {
@@ -131,7 +118,6 @@ function WibAds() {
 		_lastReloaded[adname].ival = timeout;
 		return true;
 	}
-
 	// Initiate the gallery overlay ad
 	this.set_gallery_overlay_ad = function(provider, pos, src, width, height) {
 		gallery_overlay_ad = {
@@ -142,7 +128,6 @@ function WibAds() {
 			"height": height
 		};
 	}
-
 	// Closes the gallery overlay ad. Called by ad providers from inside a fif
 	this.close_gallery_overlay = function() {
 		var wib_ad_overlay = $("#wibAdOverlay");
@@ -159,9 +144,52 @@ function wibAdsShowGalleryOverlay(from, to) {
 	wib_ads.reload("22", {"from": from ? from: 0, "to": to});
 }
 
-wib_ads = new WibAds();
-wib_ads.init();
+if ((typeof wib_ads_thirdparty == "undefined") || !wib_ads_thirdparty.got) {
+	try {
+		var l = wib_gallery_paging_slideshow_event.callbacks.length;
+		if (l) {
+			for (var p in wib_gallery_paging_slideshow_event.callbacks) {
+				if (wib_gallery_paging_slideshow_event.callbacks[p] == wib_ads.reload) {
+					wib_gallery_paging_slideshow_event.callbacks.splice(p, 1);
+					break;
+				}
+			}
+		}
+		wib_ads = new WibAdsHack();
+		wib_ads.thirdparty = true;
+		wib_ads.init();
+		wib_gallery_paging_slideshow_event.registerCb(wib_ads.reload);
+	} catch (e) {
+		console.log("Can't implement wib_ads slideshow event [wib_ads.reload], interpreter says: [" + e.name + "/" + e.message + "]");
+	}
+}
 
-// Register listener
-wib_gallery_paging_slideshow_event.registerCb(wib_ads.reload);
-wib_ads.set_reloadable_ad("adfox","adcomp-18",ad_src,"240","400");
+/*/код для adfox-->
+//replace pos, width and height with actual values
+//wib_ads.set_reloadable_ad("adfox","adcomp-18",ad_src,"240","400"); //old fashion call
+var wa_prov = "adfox";
+var wa_pos = "ad30";
+var wa_wid = "200";
+var wa_ht = "300";
+var wa_geo_spt = true;
+var wa_interval = 30000;
+var wa_ad = {
+	provider: wa_prov,
+	pos: wa_pos,
+	src: ad_src,
+	width: wa_wid,
+	height: wa_ht,
+	tm_hack: true,
+	geo_spt: wa_geo_spt,
+	ival: wa_interval
+};
+if ((typeof wib_ads != "undefined") && (typeof wib_ads.thirdparty == "boolean")) wib_ads.set_reloadable_ad(wa_ad);
+else {
+	if (typeof wib_ads_thirdparty == "undefined") {
+		var wib_ads_thirdparty = {};
+		wib_ads_thirdparty.got = false;
+		wib_ads_thirdparty.ads = [];
+	}
+	wib_ads_thirdparty.ads.push(wa_add);
+}
+//<--/*/
